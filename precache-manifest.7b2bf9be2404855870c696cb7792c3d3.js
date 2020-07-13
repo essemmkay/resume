d38db9d836aa11c0e0999ece6a471e8c39cf7424
Murtaz Kazmi <murtaz@reactivespace.com>
2020-07-13 14:26:11 +0500

    Updates

---
+++ precache-manifest.7b2bf9be2404855870c696cb7792c3d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b252c690b10c5456bf71fb2b1b678b",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "1af8b73a6ae1a727d724",
+    "url": "/resume/static/css/main.70d1ecc3.chunk.css"
+  },
+  {
+    "revision": "a4bb76a8417abecf2f3e",
+    "url": "/resume/static/js/2.dd2c67f0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/resume/static/js/2.dd2c67f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1af8b73a6ae1a727d724",
+    "url": "/resume/static/js/main.4fffc71c.chunk.js"
+  },
+  {
+    "revision": "44dff6d47c50d8cc6c93",
+    "url": "/resume/static/js/runtime-main.a1bca32c.js"
+  }
+]);
